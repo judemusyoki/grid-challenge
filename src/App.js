e9b7022491a4ee33df957f978ca8d9f237fd4dfd
jmusyoki <jmusyoki@wastehero.io>
2021-02-23 18:42:45 +0100

    created grid & search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Tile from './Tile';
 
-function App() {
+import tiles from './tiles';
+
+const App = () => {
+  // const [tiles, setTiles] = useState(tiles);
+  const [search, setSearch] = useState('');
+  const [filtered, setFiltered] = useState([]);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setFiltered(
+      tiles.filter((tile) =>
+        tile.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* {notes.map(createNote)} */}
+      <div className='create-note'>
+        <input
+          type='text'
+          label='Search grid'
+          onChange={handleChange}
+          placeholder='Search..'
+        />
+      </div>
+
+      {filtered.map((tile, idx) => (
+        <Tile
+          key={idx}
+          title={tile.title}
+          description={tile.description}
+          imagePath={tile.imagePath}
+        />
+      ))}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Tile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Tile = (props) => {
+  return (
+    <div className='tile'>
+      <img src={props.imagePath} alt={props.title}></img>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      {/* <img>{props.image}</img> */}
+    </div>
+  );
+};
+
+export default Tile;
